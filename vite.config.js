b3d22092243f4411c2c443ebd042e1dccd26066f
SURBHI SINHA <139940072+surbhisinha12@users.noreply.github.com>
2025-03-09 23:09:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/surbhisinha12/news_app.git",
+  base: "/news_app/",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
